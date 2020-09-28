ed2b226c9835e92d2130de82b1a55449b6b05811
Vanya <Vanya200373@yandex.ru>
2020-09-28 16:17:35 -0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[][] a = new int[25][4];
+        int q = 1;
+        for (int i = 0; i < 25; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (i % 2 == 0) {
+                    a[i][j] = q;
+                } else {
+                    a[i][3 - j] = q;
+                }
+                q++;
+            }
+        }
+        //iuguygyugy
+        for (int i = 0; i < 25; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
