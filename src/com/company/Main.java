22388d6ab6f0fc2c303a7b447bb9823d7200cd91
Vanya <Vanya200373@yandex.ru>
2020-09-28 16:18:22 -0200

    новый

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,6 +21,7 @@ public class Main {
             for (int j = 0; j < 4; j++) {
                 System.out.print(a[i][j] + " ");
             }
+            // новый коммент
             System.out.println(" ");
         }
     }
